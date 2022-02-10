19a41cfce354b7d66777d9ca01b2f2a2f8adb257
krisMONSTER <62694355+krisMONSTER@users.noreply.github.com>
2022-02-10 19:53:09 +0100

    Initialize project

---
+++ src/main/java/my/page/api/MyPageApiApplication.java
@@ -0,0 +1,13 @@
+package my.page.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/page/api/MyPageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package my.page.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
